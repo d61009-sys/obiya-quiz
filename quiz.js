b2e4c941ee73dba1e0e135d5c919e2caee187aa1
d61009-sys <d61009@gm.kochi-ct.jp>
2025-09-30 16:17:14 +0900

    First commit: Initial quiz files

---
+++ quiz.js
@@ -0,0 +1,73 @@
+// quiz.js
+
+const QUIZ_DATA = [
+    {
+        id: 'quiz_1',
+        title: '帯ぶら音クイズ',
+        question: 'この音はなんでしょう？',
+        options: [
+            'コピー機',
+            '印刷機', // 正解
+            'コピー機',
+            'コピー機'
+        ],
+        answer: '印刷機',
+        type: 'text' // 問題タイプ
+    },
+    {
+        id: 'quiz_2',
+        title: '帯ぶら音クイズ2',
+        question: 'この音は何でしょう？',
+        options: [
+            'ひよこ',
+            'すずめ',
+            'いんこ', // 正解
+            'メジロ'
+        ],
+        answer: 'インコ(メガネの門田)',
+        audio_src: 'assets/audio/quiz_sound_1.mp3', // 音声ファイルのパス
+        type: 'audio' // 問題タイプ
+    },
+    {
+        id: 'quiz_3',
+        title: '帯ぶら音クイズ3',
+        question: 'この音は何でしょう？',
+        options: [
+            '0',
+            '1',
+            '2', // 正解
+            '3'
+        ],
+        answer: '2',
+        type: 'text'
+    },
+    {
+        id: 'quiz_4',
+        title: '帯ぶら音クイズ4',
+        question: 'この音はなんでしょう？',
+        options: [
+            '0',
+            '1',
+            'ヤマサキ洋装店', // 正解
+            '2'
+        ],
+        answer: 'ヤマサキ洋装店',
+        type: 'text'
+    },
+    {
+        id: 'quiz_5',
+        title: '帯ぶら音クイズ5',
+        question: 'この音は何でしょう？',
+        options: [
+            '0',
+            '1', // 正解
+            '2',
+            '3'
+        ],
+        answer: '1',
+        type: 'text'
+    }
+];
+
+// 選択されたクイズを保存する変数
+let selectedQuiz = null;
